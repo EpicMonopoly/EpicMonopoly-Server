9fbb81041b225ee1c2897b4ccc24cc8abac76b8e
nixizi <11510602@mail.sustc.edu.cn>
2017-11-19 23:37:44 +0800

    Update main

--- Game_Logic/main.py
+++ Game_Logic/main.py
@@ -1,37 +1,67 @@
+import random
+import board
+
+# chess_board = board.board()
+chess_board = None
 
 def trade():
     pass
 
-def roll():
-    pass
+def roll(player):
+    """
+    Roll a dice
+        :param player: The player who roll dices
+    Returns:
+        :int: The number of first dice
+        :int: The number of second dice
+        :bool: The station of end_flag
+    """
+    print("Rolling")
+    a = random.randint(1, 6)
+    b = random.randint(1, 6)
+    if a == b:
+        end_flag = False
+    else:
+        end_flag = True
+    print("Dice number is", a, b)
+    step = a + b
+    player.move(step)
+    end_position = player.position
+    current_block = chess_board.get_block(end_position)
+    current_block.display()
+    return a, b, end_flag
 
 def construct_building():
-    pass
-
-def end_turn():
     pass
 
 def mortgage_property():
     pass
 
-def turn():
+def turn(player):
     end_flag = False
-    while end_flag is False:
+    while True:
         print("1: Trade with others")
         print("2: Roll dices")
         print("3: Construct building")
         print("4: Mortgage property")
         print("5: End turn")
-        choice = input("Please enter the number of your decision:")
+        choice = int(input("Please enter the number of your decision:"))
+        print(choice)
         if choice == 1:
             trade()
         elif choice == 2:
-            roll()
+            dice_a, dice_b, end_flag = roll(player)
         elif choice == 3:
             construct_building()
         elif choice == 4:
             mortgage_property()
         elif choice == 5:
-            end_turn()
+            if end_flag is True:
+                break
+            else:
+                print("Please roll a dice")
         else:
             print("Invalid choice")
+
+if __name__ == "__main__":
+    turn(None)
