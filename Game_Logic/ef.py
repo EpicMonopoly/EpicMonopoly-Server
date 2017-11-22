20cda3c72d97a7beb04d6bd63b80b11fdb81f80d
spacebody <510662916@qq.com>
2017-11-22 08:32:52 +0800

    update ef

--- Game_Logic/ef.py
+++ Game_Logic/ef.py
@@ -1,2 +1,23 @@
+# economy factor 
+import random
+import datetime
+
+
 class EF:
-    pass
+    """
+    Economy factor
+    """
+    def ___init___(self):
+        """
+        Constructor
+        """
+        seed = datetime.datetime.now()
+        random.seed(seed)
+        self._seed = random.randint(0, 999999)
+    
+    @property
+    def ef_value(self):
+        pass
+
+    
+    
