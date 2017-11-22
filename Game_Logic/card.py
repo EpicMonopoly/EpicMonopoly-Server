af0dc4f7ea4ebc7bcf80c571c91dffbd67eedafe
spacebody <510662916@qq.com>
2017-11-22 09:10:15 +0800

    Update card

--- Game_Logic/card.py
+++ Game_Logic/card.py
@@ -61,12 +61,12 @@ class PayCard(Card):
     def play(self, gamer, data):
         """
         :param from_role: a player or bank or rest players
-        :param from_role: player or bank or rest players
+        :param data: global game data
         """
-        to_role = None
-        from_role = gamer
         print(self.description)
         if self._card_type == 2:
+            to_role = data['epic_bank']
+            from_role = gamer
             if from_role.cash < self._amount:
                 return False
             else:
@@ -74,6 +74,12 @@ class PayCard(Card):
                 to_role.cash = self._amount
                 return True
         elif self._card_type == 8:  # need to check
+            to_role = []
+            all_role = data["player_dict"]
+            for role in all_role:
+                if role['id'] != role:
+                    to_role.append(role)
+            from_role = gamer
             total_amount = len(to_role) * self._amount
             if from_role.cash < total_amount:
                 return False
@@ -84,6 +90,7 @@ class PayCard(Card):
                 return True
         elif self._card_type == 3:
             import estate
+            from_role = gamer
             total_amount = 0
             for e in from_role.assets:
                 if isinstance(estate, estate.Estate):
@@ -119,13 +126,18 @@ class CollectCard(Card):
         :param from_role: a player or bank or rest players
         :param from_role: player or bank or rest players
         """
-        from_role = gamer
-        to_role = None
         print(self.description)
         if self._card_type == 0:
+            to_role = gamer
             to_role.cash = self._amount
             return True
         elif self._card_type == 1:
+            to_role = gamer
+            from_role = []
+            all_role = data["player_dict"]
+            for role in all_role:
+                if role['id'] != role:
+                    from_role.append(role)
             total_amount = 0
             broken_role = []
             for role in from_role:
@@ -158,10 +170,9 @@ class BailCard(Card):
         """
         Baild card, can be collected by players
         """
-        from_role = gamer
-        to_role = None
+        to_role = gamer
         print(self.description)
-        if from_role.bail_card_num == 0:
+        if to_role.bail_card_num == 0:
             print("1. Keep it yourself.")
             print("2. Sell to others.")
             while True:
@@ -172,9 +183,18 @@ class BailCard(Card):
                 except ValueError:
                     print("Please enter a number. Enter -1 to quit")
             if choice == 1:
-                from_role.bail_card_num = from_role.bail_card_num + 1
+                to_role.bail_card_num = to_role.bail_card_num + 1
             elif choice == 2:
-                # TODO: need to implement the trade
-                pass 
+                while True:
+                    input_str = input("Please enter the player of you want to sell the card to:")
+                    try:
+                        choice = str(input_str)
+                        break
+                    except ValueError:
+                        print("Please enter a player name. Enter q to quit")
+                to_role = data['player_dict'][choice]
+                from_role = gamer
+                # TODO: need to implement trade
+                pass
 
 
