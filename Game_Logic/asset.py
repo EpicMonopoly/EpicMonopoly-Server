d25fb0d3ae60c04b670e4f49a991c29d63c32a08
spacebody <510662916@qq.com>
2018-01-03 20:40:44 +0800

    Add docs for asset

--- Game_Logic/asset.py
+++ Game_Logic/asset.py
@@ -1,20 +1,33 @@
+from abc import abstractmethod
 import block
 
 
 class Asset(block.Block):
-    """
-    Class Asset
+    """Class Asset
+
+    Parameters
+    ----------
+    block.Block: superclass 
+
+    Returns
+    -------
+
     """
 
     def __init__(self, name, block_id, position, uid, estate_value, status, mortgage_rate=0.5):
-        """
-        Call constructor of superclass
-        name(super): string
-        position(super): int
-        uid: int
-        estate_value: int
-        status: int
-        street_id: int
+        """constructor
+
+        Parameters
+        ----------
+        self: class itself
+        name: asset name 
+        block_id: block id of asset
+        position: position of asset
+        uid: owner id of asset
+        estate_value: estate value of asset
+        status: status of asset
+        mortgage_rate: mortgage rate, 0.5 by default 
+
         """
         super().__init__(name, block_id, position)
         self._uid = uid
@@ -22,45 +35,109 @@ class Asset(block.Block):
         self._status = status
         self.mortgage_rate = mortgage_rate
 
-    @property  # setOwner
+    @property
     def owner(self):
+        """owner
+
+        Parameters
+        ----------
+        self: class itself
+
+        Returns
+        -------
+        uid: user id
+        """
         return self._uid
 
-    @owner.setter  # getOwner
+    @owner.setter
     def owner(self, uid):
+        """setter of owner id
+
+        Parameters
+        ----------
+        self: class itself
+        uid: owner id
+
+        """
         self._uid = uid
 
-    def change_owner(self, uid):
-        self._uid = uid
-
-    @property  # getValue
+    @property
     def value(self):
+        """estate value
+
+        Parameters
+        ----------
+        self: class itself
+
+        Returns
+        -------
+        estate_value: estate value
+        """
         return self._estate_value
 
-    @property  # getStatus
+    @property
     def status(self):
+        """status
+
+        Parameters
+        ----------
+        self: class itself
+
+        Returns
+        -------
+        status: status of asset
+        """
         return self._status
 
-    @status.setter  # setStatus
+    @status.setter
     def status(self, status):
+        """
+
+        Parameters
+        ----------
+        self: class itself
+        status: changed status
+
+        """
         self._status = status
 
-    @property  # getMortgageValue
+    @property
     def mortgage_value(self):
+        """mortagage value of asset
+
+        Parameters
+        ----------
+        self: class itself
+
+        Returns
+        -------
+        mortgage_value: mortgage value of asset
+        """
         return self._estate_value * self.mortgage_rate
 
+    @abstractmethod
     def change_value(self, rate):
-        """
-        Change ef value
-        """
         pass
 
     def payment(self):
+        """payment
+
+        Parameters
+        ----------
+        self: class itself
+
+        Returns
+        -------
+        estate_value: estate value for buying
+
+        """
         return self._estate_value
 
+    @abstractmethod
     def display(self, gamer, data_dict, dice_result):
         pass
 
+    @abstractmethod
     def getJSon(self):
         pass
         # json_data = {
